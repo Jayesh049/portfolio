fba5da8603710e1fd5cb7edba2b40caaace1d324
Joksepha <jampotom421@gmail.com>
2023-06-30 20:08:38 +0530

    added upto changed projects with links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx}",
+  './components/**/*.{js,ts,jsx,tsx}',
+'./app/**/*.{js,ts,jsx,tsx}'
+],
   mode: "jit",
   theme: {
     extend: {
